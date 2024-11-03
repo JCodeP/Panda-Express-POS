86685faaf0e144ea5b01975b214848ce427dcc23
soup-scott <campbellscott@tamu.edu>
2024-11-02 20:41:55 -0500

    confused change

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -4,33 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./Homepage/Header.jsx";
 import SiteNavBar from "./Homepage/SiteNavBar.jsx";
-<<<<<<< HEAD
-import ManagerHome from "./Managers/ManagerHome.jsx";
-import CashierHome from "./Cashiers/CashierHome.jsx";
-import CashierSubmitScreen from "./Cashiers/CashierSubmitScreen.jsx";
-import CustomerHome from "./Customers/CustomerHome.jsx";
-
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Header />
-            <SiteNavBar />
-          </>
-        }
-        />
-        <Route path="/managers" element={<ManagerHome />} />
-        <Route path="/cashiers" element={<CashierHome />} />
-        <Route path="/customers/*" element={<CustomerHome />} />
-        <Route path="/cashiers/submit" element={<CashierSubmitScreen />} />
-        <Route path="/menu" element={<h1>Menu</h1>} />
-      </Routes>
-    </Router>
-
-=======
 import CashierHome from "./Cashiers/CashierHome.jsx";
 import CashierSubmitScreen from "./Cashiers/CashierSubmitScreen.jsx";
 import ManagerHome from "./Managers/ManagerHome.jsx";
@@ -66,7 +39,6 @@ function App() {
         </Routes>
       </Router>
     </MenuProvider>
->>>>>>> a0485233bfce8ecd9b17fef47a30a6e9f601fcdf
   );
 }
 
