3c7ade300fc90003ecd16c2e684ccfce7567e5e9
soup-scott <campbellscott@tamu.edu>
2024-11-13 17:32:50 -0600

    completed preliminary frontend for combos

--- project-3/src/Customers/Menu/Combos.jsx
+++ project-3/src/Customers/Menu/Combos.jsx
@@ -1,46 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Customer.css";
 import {useNavigate} from "react-router-dom";
 import { useMenu } from "../../MenuContext";
 
 function Combos() {
-    // const [showCombos, setShowCombos] = useState(true);
-    // const chooseCombo = () => {
-    //     setShowCombos(false); // Switch to showing entrees
-    // };
+    const [showCombos, setShowCombos] = useState(true);
+    const [showEntrees, setShowEntrees] = useState(false);
+    const chooseCombo = () => {
+        setShowCombos(false); 
+    };
+    const cancelCombo = () => {
+        setShowEntrees(false);
+        setShowCombos(true); 
+    };
+    const chooseEntree = () => {
+        setShowEntrees(true);
+    };
+    const chooseSide = () => {
+        setShowEntrees(false); 
+    };
 
     const { menuItems } = useMenu();
     const comboOptions = menuItems.filter(item => item.category === "Combos");
-    const entreeOptions = menuItems.filter(item => item.category === "Entrees");
+    const { entrees } = useMenu();
+    const entreeOptions = entrees;
+    const { sides } = useMenu();
+    const sideOptions = sides;
 
     return(
-        <div className = "combo-box">
-            {(
-                comboOptions.map(combo => (
-                <button className = "combo-button" key = {comboOptions.id}>
-                    <img src="https://placehold.co/200x200" alt="placeholder" />
-                    <div className="separator" />
-                    <span>{combo.name}</span>
-                </button>
-            )))}
-        </div>
+        <>
+            
+            {showCombos ? (
+                <div className = "combo-box">
+                    {comboOptions.map(combo => (
+                    <button className = "combo-button" key = {comboOptions.id} onClick = {chooseCombo}>
+                        <img src="https://placehold.co/200x200" alt="placeholder" />
+                        <div className="separator" />
+                        <span>{combo.name}</span>
+                    </button>
+                    ))}
+                </div>
+            ) : (
+                <div className = "item-box">
+                <div className = "item-nav">
+                    <button className = "item-nav-button" onClick = {chooseSide}>
+                        Sides
+                    </button>
+                    <button className = "item-nav-button" onClick = {chooseEntree}>
+                        Entrees
+                    </button>
+                    <button className = "item-nav-button" onClick = {cancelCombo}>
+                        Cancel
+                    </button>
+                </div>
+                <div className = "item-button-box">
+                    {showEntrees ? (
+                        entreeOptions.map(entree => (
+                            <button className = "entree-button" key = {entree.id} onClick>
+                                <img src="https://placehold.co/100x100" alt="placeholder" />
+                                <div className="separator" />
+                                <span>{entree.name}</span>
+                            </button>
+                        ))
+                    ) : (
+                        sideOptions.map(side => (
+                            <button className = "side-button" key = {side.id}>
+                                <img src="https://placehold.co/100x100" alt="placeholder" />
+                                <div className="separator" />
+                                <span>{side.name}</span>
+                            </button>
+                        ))
+                    )}
+                </div>
+                </div>
+            )}
+        
+        </>
+        
         
     );
 }
 
 export default Combos;
 
-// : (
-//     entreeOptions.map(entree => (
-//         <div
-//             key={entree.id}
-//             style={{
-//                 padding: "1rem",
-//                 borderBottom: "1px solid #ddd",
-//                 textAlign: "center"
-//             }}
-//         >
-//             {entree.name}
-//         </div>
-//     ))
-// )
+
+
