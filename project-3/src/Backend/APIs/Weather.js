017ec73072b177d1cec182da1c594c651b91905c
K-August <klieder03@gmail.com>
2024-11-17 16:15:15 -0600

    Adjusted data extracted from JSON

--- project-3/src/Backend/APIs/Weather.js
+++ project-3/src/Backend/APIs/Weather.js
@@ -1,7 +1,7 @@
 
 export const fetchWeather = async () => {
     const apiKey = process.env.weatherApiKey;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=College Station&units=metric&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=College Station&units=imperial&appid=${apiKey}`;
 
     try {
         const response = await fetch(apiUrl);
@@ -9,11 +9,39 @@ export const fetchWeather = async () => {
             throw new Error(`Error fetching weather data: ${response.status}`);
         }
         const data = await response.json();
-        console.log(data);
+        
+        const desiredData = extractInfo(data);
+        console.log(desiredData);
 
-        return data; // Return the full data for debugging
+        return desiredData;
     } catch (error) {
         console.error('Error fetching weather:', error);
         throw error;
     }
+};
+
+const extractInfo = (data) => {
+    const {
+        weather: [{ main: weatherMain, description }],
+        main: { temp, feels_like, temp_min, temp_max },
+        wind: { speed: windSpeed },
+    } = data;
+
+    const highlightedInfo = {
+        weather: {
+            summary: weatherMain,
+            description: description,
+        },
+        temperature: {
+            current: temp,
+            feelsLike: feels_like,
+            min: temp_min,
+            max: temp_max,
+        },
+        wind: {
+            speed: windSpeed
+        },
+    };
+
+    return highlightedInfo;
 };
