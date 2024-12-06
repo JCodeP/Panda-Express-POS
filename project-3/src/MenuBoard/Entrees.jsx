10e9b87bcdfcc0881015c77dc2883e58315da476
K-August <klieder03@gmail.com>
2024-12-06 11:10:29 -0600

    Migrate image hosting to imgur for entrees and sides on menu board

--- project-3/src/MenuBoard/Entrees.jsx
+++ project-3/src/MenuBoard/Entrees.jsx
@@ -7,39 +7,39 @@ function Entrees() {
             <h2>Entrees</h2>
             <ul>
                 <li>
-                    <img src="/rsc/menu_icons/orange.webp" alt="Our signature dish. Crispy chicken wok-tossed in a sweet and spicy orange sauce" />
+                    <img src="https://i.imgur.com/6iVwEfs.png" alt="Our signature dish. Crispy chicken wok-tossed in a sweet and spicy orange sauce" />
                     <span>Orange Chicken</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/beijing.webp" alt="Crispy beef, bell peppers and onions in a sweet-tangy sauce" />
+                    <img src="https://i.imgur.com/nGs4uXa.png" alt="Crispy beef, bell peppers and onions in a sweet-tangy sauce" />
                     <span>Beijing Beef</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/kungpao.webp" alt="A Szechwan-inspired dish with chicken, peanuts and vegetables, finished with chili peppers" />
+                    <img src="https://i.imgur.com/lcBELCI.png" alt="A Szechwan-inspired dish with chicken, peanuts and vegetables, finished with chili peppers" />
                     <span>Kung Pao Chicken</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/walnut.webp" alt="Large tempura-battered shrimp, wok-tossed in a honey sauce and topped with glazed walnuts" />
+                    <img src="https://i.imgur.com/9zZIa3I.png" alt="Large tempura-battered shrimp, wok-tossed in a honey sauce and topped with glazed walnuts" />
                     <span>Honey Walnut Shrimp</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/teriyaki.webp" alt="Grilled chicken thigh hand-sliced to order and served with teriyaki sauce" />
+                    <img src="https://i.imgur.com/zEPSr9C.png" alt="Grilled chicken thigh hand-sliced to order and served with teriyaki sauce" />
                     <span>Grilled Teriyaki Chicken</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/broccoli.webp" alt="A classic favorite. Tender beef and fresh broccoli in a ginger soy sauce" />
+                    <img src="https://i.imgur.com/4ZwloQc.png" alt="A classic favorite. Tender beef and fresh broccoli in a ginger soy sauce" />
                     <span>Broccoli Beef</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/sweetfire.jpg" alt="Fried chicken breast pieces tossed with red peppers and pineapple" />
+                    <img src="https://i.imgur.com/qQPCCeW.jpeg" alt="Fried chicken breast pieces tossed with red peppers and pineapple" />
                     <span>SweetFire Chicken Breast</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/steak.webp" alt="Angus steak wok-seared with onions, red bell peppers and mushrooms in a savory black pepper sauce" />
+                    <img src="https://i.imgur.com/x1dPPWf.png" alt="Angus steak wok-seared with onions, red bell peppers and mushrooms in a savory black pepper sauce" />
                     <span>Black Pepper Angus Steak</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/sesame.webp" alt="Thin, crispy chicken strips with green beans and crispy yellow bell peppers with our delicious honey sesame seed sauce" />
+                    <img src="https://i.imgur.com/p8OLsll.png" alt="Thin, crispy chicken strips with green beans and crispy yellow bell peppers with our delicious honey sesame seed sauce" />
                     <span>Honey Sesame Chicken</span>
                 </li>
                 <li>
@@ -47,11 +47,11 @@ function Entrees() {
                     <span>String Bean Chicken Breast</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/mushroom.webp" alt="Chicken, mushrooms and zucchini stir-fried in a wok with a soy and ginger sauce" />
+                    <img src="https://i.imgur.com/YxcPkcd.jpeg" alt="Chicken, mushrooms and zucchini stir-fried in a wok with a soy and ginger sauce" />
                     <span>Mushroom Chicken</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/blackpepper.webp" alt="Marinated chicken, celery and onions in a bold black pepper sauce" />
+                    <img src="https://i.imgur.com/K1iPAOl.png" alt="Marinated chicken, celery and onions in a bold black pepper sauce" />
                     <span>Black Pepper Chicken</span>
                 </li>
             </ul>
--- project-3/src/MenuBoard/Sides.jsx
+++ project-3/src/MenuBoard/Sides.jsx
@@ -9,19 +9,19 @@ function Sides() {
             <h2>Sides</h2>
             <ul>
                 <li>
-                    <img src="/rsc/menu_icons/chowmein.webp" alt="Stir-fried wheat noodles with onions, celery and cabbage" />
+                    <img src="https://i.imgur.com/o1JMHr0.png" alt="Stir-fried wheat noodles with onions, celery and cabbage" />
                     <span>Chow Mein</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/friedrice.webp" alt="Steamed white rice with soy sauce, eggs, peas, carrots and green onions" />
+                    <img src="https://i.imgur.com/riymn3i.png" alt="Steamed white rice with soy sauce, eggs, peas, carrots and green onions" />
                     <span>Fried Rice</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/whiterice.webp" alt="Plain steamed rice" />
+                    <img src="https://i.imgur.com/jOAR92e.png" alt="Plain steamed rice" />
                     <span>Steamed White Rice</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/supergreens.webp" alt="Healthy combination of broccoli, carrots, cabbage & zucchini" />
+                    <img src="https://i.imgur.com/QHWRw40.png" alt="Healthy combination of broccoli, carrots, cabbage & zucchini" />
                     <span>Super Greens</span>
                 </li>
             </ul>
