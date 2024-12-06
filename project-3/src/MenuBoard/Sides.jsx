2cd0a378ee2d62442034bd79e3921f97de4d39d3
K-August <klieder03@gmail.com>
2024-12-06 10:32:50 -0600

    Added descriptive alt text to sides and appetizers in the menu board

--- project-3/src/MenuBoard/Sides.jsx
+++ project-3/src/MenuBoard/Sides.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import './MenuBoard.css';
 
+// FOOD DESCRIPTIONS FROM: https://www.pandaexpress.ca/our-food
+
 function Sides() {
     return (
         <div className="menu sides">
             <h2>Sides</h2>
             <ul>
                 <li>
-                    <img src="/rsc/menu_icons/chowmein.webp" alt="Chow Mein"/>
+                    <img src="/rsc/menu_icons/chowmein.webp" alt="Stir-fried wheat noodles with onions, celery and cabbage" />
                     <span>Chow Mein</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/friedrice.webp" alt="Fried Rice"/>
+                    <img src="/rsc/menu_icons/friedrice.webp" alt="Steamed white rice with soy sauce, eggs, peas, carrots and green onions" />
                     <span>Fried Rice</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/whiterice.webp" alt="Steamed White Rice"/>
+                    <img src="/rsc/menu_icons/whiterice.webp" alt="Plain steamed rice" />
                     <span>Steamed White Rice</span>
                 </li>
                 <li>
-                    <img src="/rsc/menu_icons/supergreens.webp" alt="Super Greens"/>
+                    <img src="/rsc/menu_icons/supergreens.webp" alt="Healthy combination of broccoli, carrots, cabbage & zucchini" />
                     <span>Super Greens</span>
                 </li>
             </ul>
