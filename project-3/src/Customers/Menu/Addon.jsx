3c034a8191a7b4a46269e5dddb1e2cdde0196999
soup-scott <campbellscott@tamu.edu>
2024-12-07 10:59:32 -0600

    React boilerplate

---
+++ project-3/src/Customers/Menu/Addon.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "../Customer.css";
+import { useMenu } from "../../MenuContext";
+import Tran from "../Translation.jsx";
+
+function Addon({addItems, language, changeLanguage}) {
+
+    return(
+        <div className = "addon-box">
+            Hi
+        </div>
+    );
+}
+
+export default Addon;
--- project-3/src/Customers/Menu/Menu.jsx
+++ project-3/src/Customers/Menu/Menu.jsx
@@ -3,6 +3,7 @@ import "../Customer.css";
 import {useNavigate} from "react-router-dom";
 import Orders from "./Orders.jsx";
 import Combos from "./Combos.jsx";
+import Addon from "./Addon.jsx";
 import Tran from "../Translation.jsx"
 
 function Menu({language, changeLanguage}) {
@@ -22,8 +23,7 @@ function Menu({language, changeLanguage}) {
                     <Tran word="Change Language" lang={language} />
                 </button>
             </>
-            
-            <div className = "addon-box"></div>
+            <Addon addItems={addItems} language={language} changeLanguage={changeLanguage} />
         </div>
         
     );
